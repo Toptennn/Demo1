dac0bfaf912180f83520defde5a54467598d08bc
Watthana Bunyarud <topten.watthana@gmail.com>
2024-04-24 15:31:00 +0700

    update1

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ const dayOfTheWeek = (date = new Date()) => {
 
     return days[date.getDay()];
 }
-//ujhuiu
+
+const monthOfTheYear = (date = new Date()) => {
+    const months = ['January', 'Februrary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    return months[date.getMonth()];
+}
 
 try {
     document.getElementById('day').innerText = dayOfTheWeek();
+    document.getElementById('month').innerText = monthOfTheYear();
 } catch(err) {}
 
-
+// 
 exports.dayOfTheWeek = dayOfTheWeek;
+exports.monthOfTheYear = monthOfTheYear;
